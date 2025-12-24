a9a6a15d265c84c4eca0e77732234180439c7ae7
MichellMelo <michellmelo58@gmail.com>
2025-12-24 18:06:19 -0300

    feat: add initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,52 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import Header from "@/components/header";
 import Image from "next/image";
+import banner from "@/public/banner.png";
+import BookingItem from "@/components/booking-item";
+import getBarbershops, { getPopularBarbershops } from "@/data/barbershops";
+import BarbershopItem from "@/components/barbershop-item";
+import {
+  PageContainer,
+  PageSectionContent,
+  PageSectionScroller,
+  PageSectionTitle,
+} from "@/components/ui/page";
+import Footer from "@/components/footer";
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await getBarbershops();
+  const popularBarbershops = await getPopularBarbershops();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button variant="destructive">Full Stack Developer</Button>
-      <Input type="email" placeholder="Email" />
+    <div>
+      <Header />
+      <PageContainer>
+        <Image
+          className="h-auto w-full"
+          src={banner}
+          alt="Agende nos melhores com a Aparatus"
+          sizes="100vw"
+        />
+        <PageSectionContent>
+          <PageSectionTitle>Agendamentos</PageSectionTitle>
+          <BookingItem />
+        </PageSectionContent>
+        <PageSectionContent>
+          <PageSectionTitle>Barbearias</PageSectionTitle>
+          <PageSectionScroller>
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </PageSectionScroller>
+        </PageSectionContent>
+
+        <PageSectionContent>
+          <PageSectionTitle>Barbearias Populares</PageSectionTitle>
+          <PageSectionScroller>
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </PageSectionScroller>
+        </PageSectionContent>
+      </PageContainer>
     </div>
   );
 }
---
+++ data/barbershops.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/prisma";
+
+const getBarbershops = async () => {
+  try {
+    const barbershop = await prisma.barbershop.findMany();
+    return barbershop;
+  } catch (error) {
+    console.error("Error fetching barbershops", error);
+    return [];
+  }
+};
+
+export default getBarbershops;
+
+export const getPopularBarbershops = async () => {
+  try {
+    const popularBarbershop = await prisma.barbershop.findMany({
+      orderBy: {
+        name: "desc",
+      },
+    });
+    return popularBarbershop;
+  } catch (error) {
+    console.error("error popular barbershop", error);
+    return [];
+  }
+};
